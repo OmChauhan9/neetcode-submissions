303840c602212047493c2ed31accea0d086c6078
OmChauhan9 <113851526+OmChauhan9@users.noreply.github.com>
2026-06-16 16:28:21 -0700

    Add: house-robber-ii - submission-5

---
+++ Data Structures & Algorithms/house-robber-ii/submission-5.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int rob(int[] nums) {
+        int[] t0 = new int[nums.length+1];
+        Arrays.fill(t0,-1);
+        int[] t1 = new int[nums.length+1];
+        Arrays.fill(t1,-1);
+
+        if(nums.length == 1) return nums[0];
+        if(nums.length <= 3) return nums.length == 2 ? Math.max(nums[0],nums[1]) : Math.max(nums[2],Math.max(nums[0],nums[1]));
+        
+        return Math.max(rMon(nums,0,0,t0),Math.max(rMon(nums,1,1,t1),rMon(nums,2,1,t1)));
+    }
+
+    public int rMon(int[] nums , int n, int index, int[] t){
+        if(index == 0 && nums.length - 1 == n) return 0;
+        if(n >= nums.length) return 0;
+        if(t[n] != -1) return t[n];
+        return t[n] = nums[n] + Math.max(rMon(nums,n+2,index,t),rMon(nums,n+3,index,t));
+    }
+}
