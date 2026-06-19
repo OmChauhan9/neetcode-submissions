bbde6db22bc82b5aed2c914470735abd227b1b12
OmChauhan9 <113851526+OmChauhan9@users.noreply.github.com>
2026-06-18 21:00:14 -0700

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        if(n == 1){
+            if(amount % coins[0] != 0) return -1;
+            else return amount/coins[0];
+        }
+
+        int[][] t = new int[n+1][amount+1];
+        return coin(coins,amount,n,t);
+    }
+
+    public int coin(int[] coins, int amount, int n, int[][] t){
+        if(n == 0 && amount == 0) return 0;
+        if(n == 0 && amount > 0) return Integer.MAX_VALUE - 1;
+
+        if(t[n][amount] != 0) return t[n][amount];
+        
+        if(coins[n-1] <= amount){
+            return t[n][amount] = Math.min(1 + coin(coins,amount - coins[n-1], n, t), coin(coins,amount,n-1,t));
+        }
+        return t[n][amount] = coin(coins,amount,n-1,t);
+    }
+}
