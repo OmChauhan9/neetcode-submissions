a09c328b88a317e52270019afd5ccd1b2866e059
OmChauhan9 <113851526+OmChauhan9@users.noreply.github.com>
2026-06-24 16:39:31 -0700

    Add: longest-increasing-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-3.java
@@ -0,0 +1,47 @@
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+//         int[][] t = new int[n+1][n+1];
+//         for(int[] t1 : t) Arrays.fill(t1,-1);
+//         return solve(nums,0,-1,t);
+//     }
+
+//     public int solve(int[] nums, int i, int p, int[][] t){
+//         if(i >= nums.length) return 0;
+//         if(t[i][p+1] != -1) return t[i][p+1];
+
+//         if(p == -1 || nums[i] > nums[p]){
+//             return t[i][p+1] = Math.max(1 + solve(nums,i+1,i,t),solve(nums,i+1,p,t));
+//         }
+//         return t[i][p+1] = solve(nums,i+1,p,t);
+//     }
+// }
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] t = new int[n];
+        Arrays.fill(t,1);
+        int max = 1;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(nums[i] > nums[j]){
+                    t[i] = Math.max(1+t[j],t[i]);
+                }
+            }
+            max = Math.max(max,t[i]);
+        }
+        return max;
+    }
+
+    // public int solve(int[] nums, int i, int p, int[][] t){
+    //     if(i >= nums.length) return 0;
+    //     if(t[i][p+1] != -1) return t[i][p+1];
+
+    //     if(p == -1 || nums[i] > nums[p]){
+    //         return t[i][p+1] = Math.max(1 + solve(nums,i+1,i,t),solve(nums,i+1,p,t));
+    //     }
+    //     return t[i][p+1] = solve(nums,i+1,p,t);
+    // }
+}
