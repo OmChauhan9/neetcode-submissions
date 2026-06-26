01917db846775bdcac03f64290d19644d6ecd715
OmChauhan9 <113851526+OmChauhan9@users.noreply.github.com>
2026-06-25 18:38:56 -0700

    Add: combination-sum-iv - submission-3

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-3.java
@@ -0,0 +1,43 @@
+// class Solution {
+//     public int combinationSum4(int[] nums, int target) {
+//         int n = nums.length;
+
+//         int[] t = new int[target+1];
+//         Arrays.fill(t,-1);
+
+//         return solve(nums, target, t);
+//     }
+
+//     public int solve(int[] nums, int target, int[] t){
+//         if(target == 0) return 1;
+//         if(target < 0) return 0;
+
+//         if(t[target] != -1) return t[target];
+
+//         int ways = 0;
+
+//         for(int i=0; i<nums.length; i++){
+//             ways += solve(nums,target - nums[i],t);
+//         }
+
+//         return t[target] = ways;
+//     }
+// }
+
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int n = nums.length;
+
+        int[] t = new int[target+1];
+        t[0] = 1;
+        
+        for(int i=1; i<=target; i++){
+            for(int j=0; j<n; j++){
+                if(i >= nums[j]){
+                    t[i] += t[i - nums[j]];
+                }
+            }
+        }
+        return t[target];
+    }
+}
