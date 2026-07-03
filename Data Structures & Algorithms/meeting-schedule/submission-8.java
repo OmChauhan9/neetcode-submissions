329c603d0ca79fa359ae75acb930db7d2031c83d
OmChauhan9 <113851526+OmChauhan9@users.noreply.github.com>
2026-07-02 21:24:09 -0700

    Add: meeting-schedule - submission-8

---
+++ Data Structures & Algorithms/meeting-schedule/submission-8.java
@@ -0,0 +1,25 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        int n = intervals.size();
+        Collections.sort(intervals, (a,b) -> Integer.compare(a.start, b.start));
+
+        for(int i=1; i<n; i++){
+            Interval previous = intervals.get(i-1);
+            Interval current = intervals.get(i);
+
+            if(current.start < previous.end) return false;
+        }
+        return true;
+    }
+}
