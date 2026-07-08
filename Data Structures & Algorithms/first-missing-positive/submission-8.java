337712c231ba06abb3cfcc656f1d0a463dba4232
OmChauhan9 <113851526+OmChauhan9@users.noreply.github.com>
2026-07-08 00:53:25 -0700

    Add: first-missing-positive - submission-8

---
+++ Data Structures & Algorithms/first-missing-positive/submission-8.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            max = Math.max(num, max);
+            min = Math.min(num, min);
+            set.add(num);
+        }
+
+        for(int i=1; i<=max; i++){
+            if(!set.contains(i)) return i;
+        }
+
+        return max < 0 ? 1 : max + 1;
+    }
+}
