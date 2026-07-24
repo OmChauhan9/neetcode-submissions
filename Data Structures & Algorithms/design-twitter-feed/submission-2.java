66b9cc3a955e68b4aaf0052ed88f3b3d3c3fcb1b
OmChauhan9 <113851526+OmChauhan9@users.noreply.github.com>
2026-07-23 19:10:08 -0700

    Add: design-twitter-feed - submission-2

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-2.java
@@ -0,0 +1,61 @@
+class Twitter {
+    HashMap<Integer, HashSet<Integer>> followMP;
+    HashMap<Integer, List<int[]>> tweetMP;
+    int count;
+
+    public Twitter() {
+        followMP = new HashMap<>();
+        tweetMP = new HashMap<>();
+        count = 0;
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+        if(!tweetMP.containsKey(userId)){
+            tweetMP.put(userId, new ArrayList<>());
+        }
+        tweetMP.get(userId).add(new int[]{count, tweetId});
+        count++;
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        List<Integer> getFeed = new ArrayList<>();
+
+        HashSet<Integer> followees = followMP.getOrDefault(userId, new HashSet<>());
+
+        for (int followeeId : followees) {
+            List<int[]> tweets = tweetMP.get(followeeId);
+            if (tweets != null) {
+                pq.addAll(tweets);
+            }
+        }
+
+        // Only add the user's own tweets if they aren't already in the followees set
+        if (!followees.contains(userId)) {
+            List<int[]> userTweets = tweetMP.get(userId);
+            if (userTweets != null) {
+                pq.addAll(userTweets);
+            }
+        }
+
+        while(getFeed.size() < 10 && pq.size() > 0){
+            int recent = pq.poll()[1];
+            getFeed.add(recent);
+        }
+
+        return getFeed;
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        if(!followMP.containsKey(followerId)){
+            followMP.put(followerId, new HashSet<>());
+        }
+        followMP.get(followerId).add(followeeId);
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        if(followMP.containsKey(followerId)){
+            followMP.get(followerId).remove(followeeId);
+        }
+    }
+}
