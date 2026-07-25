49aadad42eabe3f7d5a8cb2814aa13b07b4f4294
OmChauhan9 <113851526+OmChauhan9@users.noreply.github.com>
2026-07-24 21:15:46 -0700

    Add: rotting-fruit - submission-5

---
+++ Data Structures & Algorithms/rotting-fruit/submission-5.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        Queue<int[]> q = new ArrayDeque<>();
+        int cntFresh = 0;
+
+        for(int r=0; r<n; r++){
+            for(int c=0; c<m; c++){
+                if(grid[r][c] == 2){
+                    q.add(new int[]{r, c, 0});
+                }else if(grid[r][c] == 1){
+                    cntFresh++;
+                }
+            }
+        }
+
+        if(cntFresh == 0) return 0;
+
+        int[] delcol = {0, 1, 0, -1};
+        int[] delrow = {1, 0, -1, 0};
+        int total = 0;
+
+        while(!q.isEmpty()){
+            int[] top = q.poll();
+            int row = top[0];
+            int col = top[1];
+            int time = top[2];
+            total = Math.max(total, time);
+
+            for(int i=0; i<4; i++){
+                int newRow = row + delrow[i];
+                int newCol = col + delcol[i];
+
+                if(newRow >= 0 && newRow < n && newCol >=0 && newCol < m && grid[newRow][newCol] == 1){
+                    grid[newRow][newCol] = 2;
+                    q.add(new int[]{newRow, newCol, time + 1});
+                    cntFresh--;
+                }
+            }
+        }
+
+        return cntFresh > 0 ? -1 : total;
+    }
+}
