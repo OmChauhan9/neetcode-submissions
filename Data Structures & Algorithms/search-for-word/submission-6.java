0d12fbfe7cd75ab0534af22f2a51797b6aeae304
OmChauhan9 <113851526+OmChauhan9@users.noreply.github.com>
2026-07-24 17:53:56 -0700

    Add: search-for-word - submission-6

---
+++ Data Structures & Algorithms/search-for-word/submission-6.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int n = board.length;
+        int m = board[0].length;
+
+        for(int r=0; r<n; r++){
+            for(int c=0; c<m; c++){
+                if(dfs(r, c, board, word, n, m, 0)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean dfs(int row, int col, char[][] board, String word, int n, int m, int index){
+        if(index == word.length()) return true;
+        if(row >= n || col >= m || row < 0 || col < 0 || board[row][col] == '#' || board[row][col] != word.charAt(index)){
+            return false;
+        }
+
+        board[row][col] = '#';
+
+        boolean check = dfs(row + 1, col, board, word, n, m, index + 1) ||
+                dfs(row, col + 1, board, word, n, m, index + 1) ||
+                dfs(row -1, col, board, word, n, m, index + 1) ||
+                dfs(row, col - 1, board, word, n, m, index + 1);
+
+        board[row][col] = word.charAt(index);
+
+        return check;
+    }
+
+
+
+}
