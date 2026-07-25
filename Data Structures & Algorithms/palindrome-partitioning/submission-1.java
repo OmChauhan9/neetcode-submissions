1228643f3d06162156f73eb8a4a23b683ed90710
OmChauhan9 <113851526+OmChauhan9@users.noreply.github.com>
2026-07-24 18:07:58 -0700

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        List<String> ls = new ArrayList<>();
+        check(s, ans, ls, 0);
+        return ans;
+    }
+
+    public void check(String s, List<List<String>> ans, List<String> ls, int index){
+        if(index >= s.length()){
+            ans.add(new ArrayList<>(ls));
+            return;
+        }
+
+        for(int i=index; i<s.length(); i++){
+            if(isPalin(s.substring(index, i+1))){
+                ls.add(s.substring(index, i+1));
+                check(s, ans, ls, i + 1);
+                ls.remove(ls.size() - 1);
+            }
+        }
+    }
+
+    public boolean isPalin(String s){
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
