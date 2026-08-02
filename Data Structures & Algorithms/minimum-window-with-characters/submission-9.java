67530e0ede09ee4313c36fe20aa6ae345f08acf5
OmChauhan9 <113851526+OmChauhan9@users.noreply.github.com>
2026-08-02 11:42:01 -0700

    Add: minimum-window-with-characters - submission-9

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-9.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        if(n < m) return "";
+
+        HashMap<Character, Integer> mp = new HashMap<>();
+        for(int i=0; i<m; i++){
+            mp.put(t.charAt(i), mp.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        int l=0, r=0;
+        int sIndex = -1;
+        int minLen = Integer.MAX_VALUE;
+        int cnt = 0;
+
+        while(r < n){
+            if(mp.getOrDefault(s.charAt(r), 0) > 0){
+                mp.put(s.charAt(r), mp.getOrDefault(s.charAt(r), 0) - 1);
+                cnt++;
+            }else{
+                mp.put(s.charAt(r), mp.getOrDefault(s.charAt(r), 0) - 1);
+            }
+
+            while(cnt == m){
+                if(r - l + 1 < minLen){
+                    sIndex = l;
+                    minLen = r - l + 1;
+                }
+
+                mp.put(s.charAt(l), mp.getOrDefault(s.charAt(l), 0) + 1);
+                if(mp.getOrDefault(s.charAt(l), 0) > 0){
+                    cnt--;
+                }
+                l++;
+            }
+
+            r++;
+        }
+
+        return sIndex == -1 ? "" : s.substring(sIndex, sIndex + minLen);
+
+    }
+}
