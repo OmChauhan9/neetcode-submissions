4dccc10487ca20763fccc52eb8f3cefc55c4f062
OmChauhan9 <113851526+OmChauhan9@users.noreply.github.com>
2026-08-03 08:30:44 -0700

    Add: minimum-stack - submission-16

---
+++ Data Structures & Algorithms/minimum-stack/submission-16.java
@@ -0,0 +1,47 @@
+class MinStack {
+    Stack<Long> st;
+    long min;
+    public MinStack() {
+        st = new Stack<>();
+        min = Long.MAX_VALUE;
+    }
+    
+    public void push(int val) {
+        long v = (long) val;
+        if(st.isEmpty()){
+            min = val;
+            st.push(v);
+        }else{
+            if(v > min){
+                st.push(v);
+            }else{
+                long newVal = (2 * v) - min;
+                min = v;
+                st.push(newVal);
+            }
+        }
+    }
+    
+    public void pop() {
+        if(st.peek() > min){
+            st.pop();
+        }else{
+            long newMin = (2 * min) - st.peek();
+            min = newMin;
+            st.pop();
+        }
+    }
+    
+    public int top() {
+        long top = st.peek();
+        if(top > min){
+            return (int)top;
+        }
+
+        return (int)min;
+    }
+    
+    public int getMin() {
+        return (int)min;
+    }
+}
