9e89173c3b57b5c9bc68d13b502158c31c4b186b
OmChauhan9 <113851526+OmChauhan9@users.noreply.github.com>
2026-08-05 12:36:37 -0700

    Add: serialize-and-deserialize-binary-tree - submission-6

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-6.java
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if(root == null) return "";
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        StringBuilder sb = new StringBuilder();
+
+        while(!q.isEmpty()){
+            TreeNode top = q.poll();
+            if(top == null){
+                sb.append("#,");
+                continue;
+            }
+            sb.append(top.val + ",");
+            q.add(top.left);
+            q.add(top.right);
+        }
+
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if(data == "") return null;
+
+        String[] values = data.split(",");
+        Queue<TreeNode> q = new LinkedList<>();
+        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
+        q.add(root);
+
+        int i = 1;
+        while(!q.isEmpty()){
+            TreeNode top = q.poll();
+
+            if(!values[i].equals("#")){
+                TreeNode left = new TreeNode(Integer.parseInt(values[i]));
+                top.left = left;
+                q.add(left);
+            }
+
+            i++;
+
+            if(!values[i].equals("#")){
+                TreeNode right = new TreeNode(Integer.parseInt(values[i]));
+                top.right = right;
+                q.add(right);
+            }
+
+            i++;
+        }
+
+        return root;
+
+    }
+}
