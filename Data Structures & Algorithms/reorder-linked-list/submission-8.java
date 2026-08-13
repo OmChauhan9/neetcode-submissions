c835dca77fc241b4422e87112c0ba4d41b5bc448
OmChauhan9 <113851526+OmChauhan9@users.noreply.github.com>
2026-08-13 09:08:28 -0700

    Add: reorder-linked-list - submission-8

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-8.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode temp = slow.next;
+        ListNode prev = slow.next = null;
+
+        while(temp != null){
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+
+        ListNode temp1 = head;
+        ListNode temp2 = prev;
+
+        while(temp2 != null){
+            ListNode front1 = temp1.next;
+            temp1.next = temp2;
+            temp1 = front1;
+
+            ListNode front2 = temp2.next;
+            temp2.next = temp1;
+            temp2 = front2;
+        }
+    }
+}
